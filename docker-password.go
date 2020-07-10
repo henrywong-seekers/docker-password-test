0a0df031334a360c82b77327f842db75548b1768
whs <hwong@seekerscapital.com>
2020-07-10 11:51:53 +0800

    Add get caller identity

--- docker-password.go
+++ docker-password.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 func main() {
@@ -16,6 +18,17 @@ func main() {
 	}
 
 	cfg.Region = "ap-southeast-1"
+
+	svc0 := sts.New(cfg)
+
+	req0 := svc0.GetCallerIdentityRequest(&sts.GetCallerIdentityInput{})
+
+	result0, err := req0.Send(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result0)
 
 	svc := ecr.New(cfg)
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/henrywong-seekers/docker-password-test
 
 go 1.14
 
-require github.com/aws/aws-sdk-go-v2 v0.23.0 // indirect
+require (
+	github.com/aws/aws-sdk-go v1.33.5 // indirect
+	github.com/aws/aws-sdk-go-v2 v0.23.0 // indirect
+)
